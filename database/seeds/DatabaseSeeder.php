93631129bc680fa6e1df49eeb34e7bed5b4c9f73
Bice93 <bianca.cerulo@hotmail.it>
2022-09-22 16:20:50 +0200

    Add the content in the pivot table

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
             CategoriesTableSeeder::class,
             PostsTableSeeder::class,
             TagsTableSeeder::class,
+            PostTagTableSeeder::class,
         ]);
     }
 }
