03db6ae79dd60f17c1366e72004a0fd2eae667df
Bice93 <bianca.cerulo@hotmail.it>
2022-09-23 15:13:57 +0200

    Add CategoryController

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,7 @@ Route::middleware('auth')
     ->group(function() {
         Route::get('/', 'HomeController@index')->name('home');
         Route::resource('/posts', 'PostController');
+        Route::resource('/categories', 'CategoryController');
     });
 
 // Route::middleware('auth')->get('/home', 'Admin\HomeController@index')->name('admin.home');
