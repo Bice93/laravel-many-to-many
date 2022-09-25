e1374ab753d72d207053a078948d4b9f007106c0
Bice93 <bianca.cerulo@hotmail.it>
2022-09-25 15:35:56 +0200

    Create TagController

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,8 @@ Route::middleware('auth')
         Route::get('/', 'HomeController@index')->name('home');
         Route::resource('/posts', 'PostController');
         Route::resource('/categories', 'CategoryController');
+        Route::resource('/tags', 'TagController');
+
     });
 
 // Route::middleware('auth')->get('/home', 'Admin\HomeController@index')->name('admin.home');
