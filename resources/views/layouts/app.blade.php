a1cbe8a88b9f2ea34ae691908b47852f31c6f6c1
Bice93 <bianca.cerulo@hotmail.it>
2022-09-25 15:23:37 +0200

    Added categories link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,6 +39,9 @@
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('admin.posts.create') }}">New Post</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('admin.categories.index') }}">Categories</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
